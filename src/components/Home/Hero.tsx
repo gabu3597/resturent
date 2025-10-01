90193e7803706e9c14dc4e0e98bf8e4e1077ec48
gabuPatel0 <gabupatel0@gmail.com>
2025-10-01 16:25:32 +0530

    hkbghjg

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -76,7 +76,7 @@ const Hero: React.FC = () => {
           <div className="col-lg-6 text-center text-lg-end overflow-hidden">
             <motion.img 
               className="img-fluid" 
-              src="../img/hero.png" 
+              src="../../assets/hero.png" 
               alt="Hero"
               initial={{ opacity: 0, scale: 1.1 }}
               animate={{ opacity: 1, scale: 1 }}
